94fbbf6fc7b2fa9f8b2f9b7c8054e26c345de27a
varadesanchita <varadesanchita@gmail.com>
2023-10-28 20:59:38 +0530

    added buttons

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.demo2 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires javafx.graphics;
 
 
     opens com.example.demo2 to javafx.fxml;
